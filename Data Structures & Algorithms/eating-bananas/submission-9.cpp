fabe9355983e4e4cbd9b307c074a8ede5a534466
HOUSSAMEELBANDOUDI <149827112+HOUSSAMEELBANDOUDI@users.noreply.github.com>
2026-05-25 20:19:49 +0100

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+      int l = 1;
+      int r  = *max_element(piles.begin(), piles.end());
+      int res = r;
+
+      while (l <= r)
+      {
+        int m = l + (r - l)/ 2;
+        long long time = 0;
+        for (int p: piles){
+            time += ceil((double)p / m);
+        }
+        if (time <= h)
+        {
+            res = m;
+            r = m - 1;
+        }
+        else
+            l = m + 1;
+            
+      }
+      return res;
+        
+    }
+};
